52c0da5cb85021ffc7806c765f9ba86459460910
anreh <anurehka@gmail.com>
2019-10-21 14:03:29 +0800

    about me 2 untoil step 7

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun addNickname(view: View){
+
     }
 }
